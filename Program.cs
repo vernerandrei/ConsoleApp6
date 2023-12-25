052ddbe0f3833d9fdc776f5a4bd0d166af1b1232
Пользователь <@DESKTOP-E7N1RLV>
2023-12-25 22:08:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Найдите периметр и площадь прямоугольника");
+            //int side1 = 5;
+            //int side2 = 8;
+            //int perimeter = 2 * (side1 + side2);
+            //int square = side1 * side2;
+            //Console.ReadKey();
+
+
+            //double  catet1 = 3; 
+            //double  catet2 = 4;
+            //double  hypotenuse = Math.Sqrt(catet1 * catet1 + catet2 * catet2);
+            //Console.WriteLine($"Гипотенуза: {hypotenuse}"); 
+
+
+            //Целочисленный тип данных
+            //int side = 5; 
+
+            //int perimeter = 4 * side; 
+            //int square = side * side; 
+            //Console.WriteLine($"Периметр квадрата: {perimeter}");
+            //Console.WriteLine($"Площадь квадрата: {square}");
+
+            //Вещественный тип данных
+            //double side = 5.0; 
+            //double perimeter = 4 * side; 
+            //double square = side * side; 
+            //Console.WriteLine($"Периметр квадрата: {perimeter}");
+            //Console.WriteLine($"Площадь квадрата: {square}");
+
+
+            //Console.WriteLine("Введите двузначное число:");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number >= 10 && number <= 99)
+            //{
+            //    int units = number % 10; // Единицы
+            //    int tens = number / 10; // Десятки
+            //    Console.WriteLine($"Единицы: {units}");
+            //    Console.WriteLine($"Десятки: {tens}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Вы ввели не двузначное число.");
+
+
+
+
+            }
+       
+    }
+}
